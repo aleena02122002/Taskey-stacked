e52db1779751875a8791060be68ebfd2eaf122c9
aleena02122002 <khananna919@gmail.com>
2024-07-18 01:13:44 +0500

    textfeild added

--- lib/views/editProfile/editProfile.dart
+++ lib/views/editProfile/editProfile.dart
@@ -10,7 +10,51 @@ class EditProfile extends StatelessWidget {
     return ViewModelBuilder.reactive(
         viewModelBuilder: () => EditprofileViewmodel(),
         builder: (context, viewmodel, index) {
-          return Scaffold();
+          return Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              title: const Text(
+                "Edit Profile",
+                style:
+                TextStyle(color: Colors.white, fontFamily: "Poppins"),
+              ),
+              titleSpacing: 60,
+              leading: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+              ),
+              actions: [
+                TextButton(onPressed: (){}, child: Text("Save",style:
+                TextStyle(color: Color(0xFF3580FF), fontFamily: "Poppins",fontSize: 14,fontWeight: FontWeight.w500),))
+              ],
+            ),
+
+          );
         });
+  }
+  _textFeild(String text, TextEditingController controllers){
+    return SizedBox(
+      width: 327,
+      height: 60,
+      child: TextFormField(
+        controller: controllers,
+        decoration:  InputDecoration(
+          hintText: text,
+          hintStyle: TextStyle(color: Color(0xFF848A94)),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            borderSide: BorderSide(color: Color(0xFF191D30)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            borderSide: BorderSide(
+              color: Color(0xFF3580FF),
+              width: 1.0,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
